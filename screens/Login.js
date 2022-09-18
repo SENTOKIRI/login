bde6b3760c0f40006448006977f8b3da9865e732
Hoàn <113653261+SENTOKIRI@users.noreply.github.com>
2022-09-18 15:41:56 +0700

    log-in

--- screens/Login.js
+++ screens/Login.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button,TextInput,T } from 'react-native';
-import { Screen } from 'react-native-screens';
+
 
 export default function App() {
   
